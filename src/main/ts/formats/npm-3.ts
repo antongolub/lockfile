b96c9dd0eaa85730d1695be77f6fc1cfddd2e428
Anton Golub <antongolub@antongolub.com>
2023-08-14 21:26:59 +0300

    test: add npm lockfiles v2 and v3 testing fixtures for monorepos

---
+++ src/main/ts/formats/npm-3.ts
@@ -0,0 +1,12 @@
+// import {TSnapshot} from './interface'
+//
+// export const parse = async (value: string): Promise<TSnapshot> => {
+//     return {
+//         format: 'npm-3',
+//         entries: {}
+//     }
+// }
+//
+// export const format = async (snap: TSnapshot): Promise<string> => {
+//     return ''
+// }
